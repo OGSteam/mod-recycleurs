bc4da0ba9fd70b47794390b8ef043eb1573d7c99
Anthony <darknoon29@users.noreply.github.com>
2016-07-06 22:12:02 +0200

    Reformatage Code

--- razr.php
+++ razr.php
@@ -1,33 +1,27 @@
-
 <?php
 /***************************************************************************
-*	filename	: supp.php		
-*	desc.		: fichier suppression
-*	Author : DeusIrae
-*	created : 25/08/07
-*	modified	: 03/12/2007 
-***************************************************************************/
+ *    filename    : supp.php
+ *    desc.        : fichier suppression
+ *    Author : DeusIrae
+ *    created : 25/08/07
+ *    modified    : 03/12/2007
+ ***************************************************************************/
 
 if (!defined('IN_SPYOGAME')) die("Hacking attempt");
 
 global $db;
 global $table_prefix;
-define("TABLE_phalanges", $table_prefix."phalanges");
-define("TABLE_recycleurs", $table_prefix."recycleurs");
-
+define("TABLE_PHALANGES", $table_prefix . "phalanges");
+define("TABLE_RECYCLEURS", $table_prefix . "recycleurs");
 
 
 // On commence par récupérer les champs
-if(isset($pub_raz)) $raz=$pub_raz;
-else $raz="";
+if (isset($pub_raz)) $raz = $pub_raz; else $raz = "";
 
-    $query = "DELETE from " .TABLE_phalanges. "";
- $db->sql_query($query);
- 
-     $query = "DELETE from " .TABLE_recycleurs. "";
- $db->sql_query($query);
- 
- redirection("index.php?action=recycleurs&sub_action=admin");
+$query = "DELETE from " . TABLE_PHALANGES;
+$db->sql_query($query);
 
+$query = "DELETE from " . TABLE_RECYCLEURS;
+$db->sql_query($query);
 
-?>
+redirection("index.php?action=recycleurs&sub_action=admin");
--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,5 @@ if (!defined('IN_SPYOGAME')) {
 
 global $db, $table_prefix;
 $mod_uninstall_name = "recycleurs";
-$mod_uninstall_table = $table_prefix."recycleurs".','.$table_prefix."phalanges";
-uninstall_mod ($mod_uninstall_name, $mod_uninstall_table);
-?>
+$mod_uninstall_table = $table_prefix . "recycleurs" . ',' . $table_prefix . "phalanges";
+uninstall_mod($mod_uninstall_name, $mod_uninstall_table);
