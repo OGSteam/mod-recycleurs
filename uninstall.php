85f3c5289b7283d9dd923aee2f977d2e9c723693
Anthony <darknoon29@users.noreply.github.com>
2016-07-08 01:01:56 +0200

    Ajout Callback dans les scripts d'installation

--- uninstall.php
+++ uninstall.php
@@ -4,6 +4,30 @@ if (!defined('IN_SPYOGAME')) {
 }
 
 global $db, $table_prefix;
+
+define("TABLE_XTENSE_CALLBACKS", $table_prefix . "xtense_callbacks");
+
+// On récupère l'id du mod pour xtense...
+$query = "SELECT id FROM ".TABLE_MOD." WHERE action='recycleurs'";
+$result = $db->sql_query($query);
+$mod_id= $db->sql_fetch_row($result);
+$mod_id = $mod_id[0];
+
+// On regarde si la table xtense_callbacks existe :
+$query = 'SHOW TABLES LIKE "'.TABLE_XTENSE_CALLBACKS.'"';
+$result = $db->sql_query($query);
+if ($db->sql_numrows($result) != 0) {
+    //Le mod xtense 2 est installé !
+    //Maintenant on regarde si cdr est dedans normalement oui mais on est jamais trop prudent...
+    $query = 'SELECT * FROM '.TABLE_XTENSE_CALLBACKS.' WHERE mod_id = '.$mod_id;
+    $result = $db->sql_query($query);
+    if ($db->sql_numrows($result) != 0) {
+        // Il est  dedans alors on l'enlève :
+        $query = 'DELETE FROM '.TABLE_XTENSE_CALLBACKS.' WHERE mod_id = '.$mod_id;
+        $db->sql_query($query);
+    }
+}
+
 $mod_uninstall_name = "recycleurs";
 $mod_uninstall_table = $table_prefix . "recycleurs" . ',' . $table_prefix . "phalanges";
 uninstall_mod($mod_uninstall_name, $mod_uninstall_table);
--- update.php
+++ update.php
@@ -16,3 +16,25 @@ global $db, $table_prefix;
 $mod_folder = "recycleur";
 $mod_name = "recycleurs";
 update_mod($mod_folder, $mod_name);
+
+define("TABLE_XTENSE_CALLBACKS", $table_prefix . "xtense_callbacks");
+
+// Insertion de la liaison entre Xtense v2 et cdr
+// Quelle est l'ID du mod ?
+// On récupère le n° d'id du mod
+$query = "SELECT `id` FROM `" . TABLE_MOD . "` WHERE `action`='recycleurs' AND `active`='1' LIMIT 1";
+$result = $db->sql_query($query);
+$mod_id = $db->sql_fetch_row($result);
+$mod_id = $mod_id[0];
+
+// On regarde si la table xtense_callbacks existe :
+$result = $db->sql_query('SHOW tables LIKE "' . TABLE_XTENSE_CALLBACKS . '"');
+if ($db->sql_numrows($result) != 0) {
+    // Maintenant on regarde si recycleurs est dedans
+    $result = $db->sql_query("SELECT * FROM " . TABLE_XTENSE_CALLBACKS . " WHERE mod_id = '$mod_id'");
+    $nresult = $db->sql_numrows($result);
+
+    // S'il n'y est pas : alors on l'ajoute !
+    if ($nresult == 0) $db->sql_query("INSERT INTO " . TABLE_XTENSE_CALLBACKS . " (mod_id, function, type, active) VALUES ('" . $mod_id . "', 'recycleurs_import', 'fleet', 1)");
+    echo "<script>alert('" . $lang['xtense_ok'] . "')</script>";
+}
