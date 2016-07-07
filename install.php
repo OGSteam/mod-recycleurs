7240a7eea3a0f744383f2e00a51d511586409443
darknoon29 <darknoon@darkcity.fr>
2016-07-08 01:01:56 +0200

    Ajout Callback dans les scripts d'installation

--- install.php
+++ install.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Fichier installation du mod recycleurs
+ *
  * @author deusirae
  * @package recycleurs
  * @version 1.0a
  * @link http://ogsteam.fr
  **/
-require_once("common.php");
 
 //L'appel direct est interdit
 if (!defined('IN_SPYOGAME')) die("Hacking attempt");
@@ -18,11 +18,12 @@ $is_ok = install_mod($mod_folder);
 if ($is_ok == true) {
     define("TABLE_RECYCLEURS", $table_prefix . "recycleurs");
     define("TABLE_PHALANGES", $table_prefix . "phalanges");
+    define("TABLE_XTENSE_CALLBACKS", $table_prefix . "xtense_callbacks");
 
-    $query = "DROP TABLE IF EXISTS " . TABLE_PHALANGES . "";
+    $query = "DROP TABLE IF EXISTS " . TABLE_PHALANGES;
     $db->sql_query($query);
 
-    $query = "DROP TABLE IF EXISTS " . TABLE_RECYCLEURS . "";
+    $query = "DROP TABLE IF EXISTS " . TABLE_RECYCLEURS;
     $db->sql_query($query);
 
 
@@ -51,7 +52,28 @@ if ($is_ok == true) {
 			PRIMARY KEY ( `id` ) 
 		)";
     $db->sql_query($query);
+
+    // Insertion de la liaison entre Xtense v2 et cdr
+// Quelle est l'ID du mod ?
+    // On récupère le n° d'id du mod
+    $query = "SELECT `id` FROM `" . TABLE_MOD . "` WHERE `action`='recycleurs' AND `active`='1' LIMIT 1";
+    $result = $db->sql_query($query);
+    $mod_id = $db->sql_fetch_row($result);
+    $mod_id = $mod_id[0];
+    log_('debug',$mod_id);
+// On regarde si la table xtense_callbacks existe :
+    $result = $db->sql_query('SHOW tables LIKE "' . TABLE_XTENSE_CALLBACKS . '"');
+    if ($db->sql_numrows($result) != 0) {
+        // Maintenant on regarde si recycleurs est dedans
+        $result = $db->sql_query("SELECT * FROM " . TABLE_XTENSE_CALLBACKS . " WHERE mod_id = '$mod_id'");
+        $nresult = $db->sql_numrows($result);
+
+        // S'il n'y est pas : alors on l'ajoute !
+
+        if ($nresult == 0) $db->sql_query("INSERT INTO " . TABLE_XTENSE_CALLBACKS . " (mod_id, function, type, active) VALUES ('" . $mod_id . "', 'recycleurs_import', 'fleet', '1')");
+    }
+
+
 } else {
     echo "<script>alert('Désolé, un problème a eu lieu pendant l'installation, corrigez les problèmes survenue et réessayez.');</script>";
 }
-?>
