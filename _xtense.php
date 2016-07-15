2abaa3e9c301d511b4291f6461ee60a769ec7c86
Anthony <darknoon29@users.noreply.github.com>
2016-07-15 16:39:19 +0200

    Import des donn?es de phalanges via la callback Xtense

--- _xtense.php
+++ _xtense.php
@@ -14,7 +14,9 @@ if (!defined('IN_SPYOGAME')) die("Hacking attempt");
 global $db, $table_prefix, $user, $xtense_version;
 $xtense_version = "2.6.0";
 
-define("TABLE_RECYCLEURS", $table_prefix . "recycleurs");
+require_once("./mod/recycleurs/core/phalanx.php");
+require_once("./mod/recycleurs/core/recyclers.php");
+
 
 // TEST XTENSE2
 
@@ -39,12 +41,21 @@ if (class_exists("Callback")) {
                 return Io::ERROR;
         }
 
+        public function phalanx_import($system)
+        {
+            global $io;
+            if (phalanx_import($system))
+                return Io::SUCCESS;
+            else
+                return Io::ERROR;
+        }
+
         /**
          * @return array
          */
         public function getCallbacks()
         {
-            return array(array('function' => 'recycleurs_import', 'type' => 'fleet'));
+            return array(array('function' => 'recycleurs_import', 'type' => 'fleet'), array('function' => 'phalanx_import', 'type' => 'buildings'));
         }
     }
 }
@@ -54,8 +65,7 @@ if (class_exists("Callback")) {
  */
 function recycleurs_import($data)
 {
-    global  $user_data, $db, $table_prefix;
-
+    global $user_data, $db, $table_prefix;
 
     // données a traiter
     // timestamp actuel
@@ -66,28 +76,35 @@ function recycleurs_import($data)
 
     $isMoon = $data['planet_type'];
     $planet_name = $data['planet_name'];
-    $coordinates = $player_galaxy.":".$player_system.":".$player_position;
+    $coordinates = $player_galaxy . ":" . $player_system . ":" . $player_position;
     $nb_recycleurs = $data['fleet']['REC'];
 
     $required_recy = mod_get_option('recy_limit');
-    if(mod_get_option('recy_limit') < 1) $required_recy = 1;
+    if (mod_get_option('recy_limit') < 1) $required_recy = 1;
 
-    if($nb_recycleurs > $required_recy){
+    if ($nb_recycleurs > $required_recy) {
 
         //On vérifie si il y a une porte de saut à proximité (La porte n'est dispo que sur les lunes)
-        $request = "SELECT `planet_name` FROM ".TABLE_USER_BUILDING." WHERE  `PoSa` = '1' AND `coordinates` = ".$coordinates;
-        $posa = $db->sql_numrows($request);
+        $request = "SELECT `planet_name` FROM " . TABLE_USER_BUILDING . " WHERE  `PoSa` = '1' AND `coordinates` = '" . $coordinates . "'";
+        $posa = $db->sql_numrows($db->sql_query($request));
 
-        $request="SELECT `galaxie` , `systeme` , `position`, `nombrerecy` from ".TABLE_RECYCLEURS." WHERE `galaxie`=".$player_galaxy." AND systeme=".$player_system." AND position='".$player_position."'";
-
-        if($db->sql_numrows($db->sql_query($request)) != 0)
-            $query = "UPDATE ".TABLE_RECYCLEURS." SET `porte`= '".$posa."', `nombrerecy`= '".$nb_recycleurs."', `time` = '".$date."' WHERE `galaxie`= '".$player_galaxy."' AND `systeme`='".$player_system."' AND `position` = '".$player_position."'";
-        else
-            $query = "INSERT INTO " . TABLE_RECYCLEURS . "(`id` , `user_name` , `galaxie` , `systeme` , `position` , `porte` , `nombrerecy` , `time`) VALUES ('', '" . $user_data['user_name'] . "', '".$player_galaxy."', '".$player_system."', '".$player_position."', '".$posa."', '".$nb_recycleurs."', '" . $date . "')";
-
-
-        $db->sql_query($query);
+        add_recyclers($player_galaxy, $player_system, $player_position, $posa, $nb_recycleurs, true);
     }
 
+    return true;
+}
+
+function phalanx_import($data)
+{
+    $player_galaxy = $data['coords'][0];
+    $player_system = $data['coords'][1];
+    $player_position = $data['coords'][2];
+
+    if (isset($data['buildings']['Pha'])) {
+        $lvl_phalange = $data['buildings']['Pha'];
+
+        if ($lvl_phalange > 0)
+            add_phalanx($player_galaxy, $player_system, $player_position, $lvl_phalange, true);
+    }
     return true;
 }
